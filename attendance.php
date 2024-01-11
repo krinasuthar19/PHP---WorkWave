6b090a49c69a2b6276046d476f83c1e4ebaaae92
YashRana2103 <yashrana21032006@gmail.com>
2024-01-11 17:36:07 +0530

    clone repo

--- attendance.php
+++ attendance.php
@@ -34,7 +34,7 @@
                             <h4 class="mb-sm-0 font-size-18">Attendance</h4>
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Minia</a></li>
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">WorkWave</a></li>
                                     <li class="breadcrumb-item active">Attendance</li>
                                 </ol>
                             </div>
