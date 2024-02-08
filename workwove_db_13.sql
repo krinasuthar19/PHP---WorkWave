7e798f05d98aaaa0f5a218166b306fc620627428
jpviradiya <jeelviradiya188@gmail.com>
2024-02-08 15:40:50 +0530

    db

---
+++ workwove_db_13.sql
@@ -0,0 +1,436 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 08, 2024 at 11:09 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `workwave_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign_task`
+--
+
+CREATE TABLE `assign_task` (
+  `id` int(11) NOT NULL,
+  `u_id` int(20) NOT NULL,
+  `t_id` int(11) NOT NULL,
+  `d_id` int(20) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assign_task`
+--
+
+INSERT INTO `assign_task` (`id`, `u_id`, `t_id`, `d_id`, `status`) VALUES
+(2, 1026, 17, 5, ''),
+(3, 1027, 17, 5, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `d_id` int(20) NOT NULL,
+  `d_name` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`d_id`, `d_name`) VALUES
+(1, 'Department 1'),
+(4, 'IT'),
+(5, 'Billing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_attendance`
+--
+
+CREATE TABLE `employee_attendance` (
+  `id` int(11) NOT NULL,
+  `emp_id` int(20) NOT NULL,
+  `attendance_date` date NOT NULL,
+  `in_time` time DEFAULT NULL,
+  `out_time` time DEFAULT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_attendance`
+--
+
+INSERT INTO `employee_attendance` (`id`, `emp_id`, `attendance_date`, `in_time`, `out_time`, `status`) VALUES
+(8, 1026, '2024-01-01', '19:12:00', '20:00:18', 'Present'),
+(9, 1026, '2024-01-02', '18:12:00', '22:00:22', 'Present'),
+(10, 1027, '2024-01-16', '09:12:00', '19:00:23', 'Present'),
+(11, 1027, '2024-01-17', '08:12:00', '21:00:22', 'Present'),
+(13, 1027, '2024-01-17', '08:12:00', '23:00:22', 'Present'),
+(14, 1026, '2024-01-01', '19:25:00', '21:00:23', 'Present'),
+(15, 1026, '2024-01-01', '19:25:00', '23:25:00', 'Present');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leave_tbl`
+--
+
+CREATE TABLE `leave_tbl` (
+  `id` int(11) NOT NULL,
+  `staff_id` int(11) NOT NULL,
+  `leave_reason` varchar(90) NOT NULL,
+  `description` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `leave_from` date NOT NULL,
+  `leave_to` date NOT NULL,
+  `updated_on` date NOT NULL,
+  `applied_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `leave_tbl`
+--
+
+INSERT INTO `leave_tbl` (`id`, `staff_id`, `leave_reason`, `description`, `status`, `leave_from`, `leave_to`, `updated_on`, `applied_on`) VALUES
+(1, 0, 'Bimaar', 'edAC', 2, '2024-01-12', '2024-01-19', '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(20) NOT NULL,
+  `project_name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `status` char(10) NOT NULL,
+  `u_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_name`, `description`, `start_date`, `end_date`, `status`, `u_id`) VALUES
+(1, 'test', '', '0000-00-00', '0000-00-00', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `r_id` int(20) NOT NULL,
+  `r_name` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`r_id`, `r_name`) VALUES
+(1, 'admin'),
+(2, 'HR'),
+(3, 'employee'),
+(4, 'project_manager');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salaries`
+--
+
+CREATE TABLE `salaries` (
+  `salary_id` int(11) NOT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `basic_salary` decimal(10,2) NOT NULL,
+  `bonus` decimal(10,2) NOT NULL,
+  `total_salary` decimal(10,2) NOT NULL,
+  `payment_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `t_id` int(11) NOT NULL,
+  `t_title` varchar(255) NOT NULL,
+  `t_description` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`t_id`, `t_title`, `t_description`, `department`, `start_date`, `end_date`, `status`) VALUES
+(10, 'Parva', 'Parva ne kaam karavo', 'Department 2', '2024-01-01', '2024-01-31', ''),
+(11, 'Prviiii', 'eqhuadsghxuhaxuaUP', 'Department 1', '2024-01-25', '2024-01-01', ''),
+(13, 'Run', 'Bhagooo', 'IT', '2024-01-02', '2024-01-03', '2'),
+(15, 'HW', 'complete HW', 'IT', '2024-01-01', '2024-01-31', '0'),
+(17, 'Project', 'Complete Project', 'Billing', '2024-01-01', '2024-01-31', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin'),
+(2, 'krina', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `u_id` int(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_image` varchar(255) NOT NULL,
+  `firstname` char(20) NOT NULL,
+  `lastname` char(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `dob` date NOT NULL,
+  `role` int(20) NOT NULL,
+  `d_id` int(20) NOT NULL,
+  `salary` int(5) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `pincode` int(20) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `session_token` varchar(255) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `date_of_joining` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`u_id`, `username`, `password`, `profile_image`, `firstname`, `lastname`, `email`, `phone`, `dob`, `role`, `d_id`, `salary`, `address`, `city`, `state`, `pincode`, `country`, `session_token`, `gender`, `date_of_joining`) VALUES
+(1026, 'Yash Rana', '$2y$10$ci6nOPhlpJBqr', 'profile_images/img_65a91c2cb784a.jpg', 'Yash', 'Rana', 'yashrana123@gmail.com', 1234567899, '2024-01-01', 3, 4, 0, 'LJ', 'Ahmedabad', 'Gujarat', 380061, 'India', '', 'male', '2024-01-01'),
+(1027, 'Jeel Viradiya', '$2y$10$m0JpDd8Op5Evx', 'profile_images/img_65a91c68a0b82.jpg', 'Jeel', 'Viradiya', 'jeelvir123@gmail.com', 2147481258, '2024-01-01', 4, 5, 0, 'LJ', 'Ahmedabad', 'Gujarat', 380051, 'India', '', 'male', '2024-01-02'),
+(1029, 'Neha Soni', '$2y$10$g6/iI5dfDN8aB', 'profile_images/img_65acad197b00f.jpg', 'Neha', 'Soni', 'nehasoni@gmail.com', 2147483647, '2024-01-01', 4, 1, 0, 'ahm', 'ahm', 'guj', 333333, 'ind', '', 'female', '2024-01-16'),
+(1030, 'parva shah', '$2y$10$rTYhKZfmfcZPS', 'profile_images/img_65ace50e832fe.jpg', 'parva', 'shah', 'parvi@gmail.com', 1234567890, '2024-01-23', 1, 4, 0, 'RAJ LAXMI PARK', 'Dhoraji (M)', 'Dhoraji,Rajkot,GJ', 360410, 'India', '', 'male', '2024-02-06'),
+(1032, 'krina suthar', '123456', 'profile_images/img_65acef7ebe734.jpg', 'krina', 'suthar', 'krina@gmail.com', 2147483647, '2024-02-29', 3, 5, 0, 'qwer', 'asdf', 'zxcv', 123654, 'poiu', '', 'female', '2024-05-29'),
+(1033, 'aaa aaa', '11111', 'profile_images/img_65acf179417e3.jpg', 'aaa', 'aaa', 'aaa@gmail.com', 1111111111, '2024-01-06', 3, 4, 0, 'aaa', 'aaa', 'aaa', 111111, 'aaa', '', 'male', '2023-12-31'),
+(1034, 'qqq q', 'qqqq1111', 'profile_images/img_65acfa44da644.jpg', 'qqq', 'q', 'qqq@gmail.com', 1111111111, '2024-01-12', 1, 4, 0, 'q', 'q', 'q', 123465, 'q', '', 'male', '2024-02-01'),
+(1035, 'ewefweff wefewfw', 'ewefweffwefewfw7890', 'profile_images/user_default_img.jpg', 'ewefweff', 'wefewfw', 'wefewf@gmail.com', 1234567890, '2024-01-06', 3, 1, 0, 'q', 'q', 'q', 124563, 'q', '', 'female', '2024-01-17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assign_task`
+--
+ALTER TABLE `assign_task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `u_id` (`u_id`),
+  ADD KEY `t_id` (`t_id`),
+  ADD KEY `d_id` (`d_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`d_id`);
+
+--
+-- Indexes for table `employee_attendance`
+--
+ALTER TABLE `employee_attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user ref` (`emp_id`);
+
+--
+-- Indexes for table `leave_tbl`
+--
+ALTER TABLE `leave_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`r_id`);
+
+--
+-- Indexes for table `salaries`
+--
+ALTER TABLE `salaries`
+  ADD PRIMARY KEY (`salary_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`),
+  ADD KEY `role ref` (`role`),
+  ADD KEY `department ref` (`d_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assign_task`
+--
+ALTER TABLE `assign_task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `d_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `employee_attendance`
+--
+ALTER TABLE `employee_attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `leave_tbl`
+--
+ALTER TABLE `leave_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `r_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `salaries`
+--
+ALTER TABLE `salaries`
+  MODIFY `salary_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1036;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assign_task`
+--
+ALTER TABLE `assign_task`
+  ADD CONSTRAINT `t_id` FOREIGN KEY (`t_id`) REFERENCES `task` (`t_id`),
+  ADD CONSTRAINT `u_id` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);
+
+--
+-- Constraints for table `employee_attendance`
+--
+ALTER TABLE `employee_attendance`
+  ADD CONSTRAINT `user ref` FOREIGN KEY (`emp_id`) REFERENCES `users` (`u_id`);
+
+--
+-- Constraints for table `salaries`
+--
+ALTER TABLE `salaries`
+  ADD CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `department ref` FOREIGN KEY (`d_id`) REFERENCES `department` (`d_id`),
+  ADD CONSTRAINT `role ref` FOREIGN KEY (`role`) REFERENCES `role` (`r_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
