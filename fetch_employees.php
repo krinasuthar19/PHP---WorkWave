5e091b0753902f56e5cb8d42bfdfe1d6ac812745
Yash <yashrana21032006@gmail.com>
2024-02-11 19:28:04 +0530

    att ki mkc

--- fetch_employees.php
+++ fetch_employees.php
@@ -5,13 +5,16 @@ include 'layouts/config.php';
 // Check if departmentId is set in the POST request
 if (isset($_POST['departmentId'])) {
   $departmentId = $_POST['departmentId'];
-  
+
   // Perform SQL query to fetch employees based on departmentId
+  // $sql = "SELECT * FROM users WHERE d_id = $departmentId";
+  // $stmt = $link->prepare($sql);
+  // $stmt->bind_param("i", $departmentId);
+  // $stmt->execute();
+  // $result = $stmt->get_result();
   $sql = "SELECT * FROM users WHERE d_id = $departmentId";
-  $stmt = $link->prepare($sql);
-  $stmt->bind_param("i", $departmentId);
-  $stmt->execute();
-  $result = $stmt->get_result();
+  $result = $link->query($sql);
+
 
   // Fetch and store the result in an array
   $employees = array();
@@ -24,12 +27,12 @@ if (isset($_POST['departmentId'])) {
   }
 
   // Close statement and database connection
-  $stmt->close();
+  // $stmt->close();
   $link->close(); // Close connection if not needed elsewhere
 
   // Return the array of employees as JSON
   echo json_encode($employees);
-} 
+}
 // else {
 //   // If departmentId is not set in the POST request, return an error message
 //   echo json_encode(array('error' => 'Department ID not provided'));
