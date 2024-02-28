f93f8c130fccb06e2abdcf1b980bfebc3f808e60
bhakti668 <147500288+bhakti668@users.noreply.github.com>
2024-02-28 21:26:27 +0530

    emp

--- add_leave.php
+++ add_leave.php
@@ -23,7 +23,7 @@ if ($_SESSION['role'] == 3 || $_SESSION['role'] == 4) {
                 <h4 class="mb-sm-0 font-size-18">Add Leave</h4>
                 <div class="page-title-right">
                   <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">WorkWave</a></li>
+                    <!--<li class="breadcrumb-item"><a href="javascript: void(0);">WorkWave</a></li>-->
                     <li class="breadcrumb-item active">Add Leave</li>
                   </ol>
                 </div>
